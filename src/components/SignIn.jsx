c0748bad0b4f1937cac7e7a0f4e2fe7518cf61bb
Gilbert Marasigan <21050167+GilbertMarasigan@users.noreply.github.com>
2025-04-22 19:52:12 +0800

    design textboxinput

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,6 +1,29 @@
-import { View, TextInput } from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
 import { useFormik } from 'formik';
 // import Text from './Text';
+
+const styles = StyleSheet.create({
+    form: {
+        display: 'flex',
+        padding: 20,
+        justifyContent: 'center'
+    },
+    field: {
+        height: 50,
+        backgroundColor: '#fff',
+        borderColor: '#ccc',
+        borderWidth: 1,
+        borderRadius: 8,
+        paddingHorizontal: 15,
+        fontSize: 16,
+        marginBottom: 20,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+        elevation: 2
+    }
+})
 
 const initialValues = {
     username: '',
@@ -14,12 +37,14 @@ const SignInForm = ({ onSubmit }) => {
     })
 
     return (
-        <View>
+        <View style={styles.form}>
             <TextInput
+                style={styles.field}
                 placeholder='Username'
                 value={formik.values.username}
                 onChangeText={formik.handleChange('username')} />
             <TextInput
+                style={styles.field}
                 placeholder='Password'
                 value={formik.values.password}
                 secureTextEntry={true}
