7c7f21e736443e22d8490b9accfd5e70ce411e0d
Gilbert Marasigan <21050167+GilbertMarasigan@users.noreply.github.com>
2025-04-25 20:32:37 +0800

    fix graphql peer dependency issues

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { NativeRouter } from 'react-router-native';
+import { ApolloProvider } from '@apollo/client';
 
 import Main from './src/components/Main'
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import createApolloClient from './src/components/utils/apolloClient';
+
+const apolloClient = createApolloClient();
 
 const App = () => {
   return (
     <SafeAreaProvider>
       <NativeRouter>
-        <Main />
+        <ApolloProvider client={apolloClient}>
+          <Main />
+        </ApolloProvider>
       </NativeRouter>
       <StatusBar style='auto' />
     </SafeAreaProvider>
